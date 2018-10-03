98f563d29ed7b96e4bbcd3526a559131983b2c78
geiercc <34074212+geiercc@users.noreply.github.com>
2018-10-03 08:40:38 -0700

    Add files via upload

---
+++ ShellScript.py
@@ -0,0 +1,35 @@
+def main():
+    Arabic = "Diary_50_Arabic_EditsStandardized_20150917 (1).html"
+
+    Diary = "lines.html"
+def find
+
+def replace
+
+current = Diary.readline()
+while current:
+    if current.contains(A50)
+        lineToReplace = current
+        lineToReplace = build up a string
+    else
+    current = Diary.readline()
+
+
+
+open edits standardized
+want to open with r+
+line = Diary.readLine
+filedata = filedata.replace('ram', 'abcd')
+parse the file and look for ID-cell
+after the > pick out string from > to <
+copy that string
+find and replace
+insert before the string <a id = "string"> </a>
+
+keep a list of all the ids that we created
+
+open links.html
+search for [A50_
+copy the string below
+insert before the string ->
+<a href= "Diary_50_Arabic_EditsStandardized_20150917 (1).html#A50_009_09:001">Link to arabic</A>
